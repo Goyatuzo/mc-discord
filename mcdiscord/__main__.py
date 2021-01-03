0e175a3f072a9ef2d8817b97b186b8cd3d96796a
Yuto Otaguro <yotagur@gmail.com>
2021-01-03 15:19:01 -0600

    Print player list on minecraft

--- mcdiscord/__main__.py
+++ mcdiscord/__main__.py
@@ -2,6 +2,7 @@ import discord
 from os import environ
 from dotenv import load_dotenv
 from pathlib import Path
+from mcstatus import MinecraftServer
 
 # The .env file should live in the parent directory
 load_dotenv(dotenv_path=Path('../'))
@@ -12,8 +13,14 @@ client = discord.Client()
 async def on_ready():
 	print('Bot is ready')
 
+
 @client.event
 async def on_message(message):
-	pass
+	if message.content == '!players':
+		server = MinecraftServer('10.0.0.18', 25565)
+		query = server.query()
+
+		await message.channel.send(f"Players Online: {', '.join(query.players.names)}")
+
 
 client.run(environ['MCD_BOT_TOKEN'])
