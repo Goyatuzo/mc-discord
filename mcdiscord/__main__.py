f9aa4ce5a96634f0b4d334de28405d9a7fe969db
Yuto Otaguro <yotagur@gmail.com>
2021-01-05 14:32:51 -0600

    Files from server can be read

--- mcdiscord/__main__.py
+++ mcdiscord/__main__.py
@@ -3,6 +3,7 @@ from os import environ
 from dotenv import load_dotenv
 from pathlib import Path
 from mcstatus import MinecraftServer
+from .servernet import setup
 
 # The .env file should live in the parent directory
 load_dotenv(dotenv_path=Path('../'))
@@ -11,13 +12,14 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
+	setup()
 	print('Bot is ready')
 
 
 @client.event
 async def on_message(message):
 	if message.content == '!players':
-		server = MinecraftServer('10.0.0.18', 25565)
+		server = MinecraftServer(environ['MC_SERVER_IP'], 25565)
 		query = server.query()
 
 		await message.channel.send(f"Players Online: {', '.join(query.players.names)}")
