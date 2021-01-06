30f862a0a8510f18ee9d76ed4da36670dedec2bc
Yuto Otaguro <yotagur@gmail.com>
2021-01-06 12:28:29 -0600

    Display top 3 killers

--- mcdiscord/__main__.py
+++ mcdiscord/__main__.py
@@ -3,7 +3,10 @@ from os import environ
 from dotenv import load_dotenv
 from pathlib import Path
 from mcstatus import MinecraftServer
+from operator import attrgetter
+
 from .servernet import setup
+from .players import Player
 
 # The .env file should live in the parent directory
 load_dotenv(dotenv_path=Path('../'))
@@ -23,6 +26,13 @@ async def on_message(message):
 		query = server.query()
 
 		await message.channel.send(f"Players Online: {', '.join(query.players.names)}")
+	elif message.content == '!killer':
+		players = Player.get_all_players()
+		players_by_lethality = sorted(players, key=attrgetter('total_mob_kills'), reverse=True)
 
+		top_three = players_by_lethality[:3]
+		to_respond = "\n".join([f"{player.username}: {player.total_mob_kills} mobs" for player in top_three])
+	
+		await message.channel.send(to_respond)
 
 client.run(environ['MCD_BOT_TOKEN'])
--- mcdiscord/players.py
+++ mcdiscord/players.py
@@ -1,5 +1,4 @@
 from typing import List
-from os import listdir
 from json import load as jload
 from nbtlib import load as nload
 
@@ -9,25 +8,49 @@ class Player:
 	__uuid: str
 	__username: str
 
-	def __init__(self, uuid):
+	def __init__(self, uuid, username):
 		self.__uuid = uuid
-
-	def load_data(self):
-		fpath = get_server_file("usernamecache.json")
-
-		with open(fpath, mode='r') as f:
-			username_cache = jload(f)
-			self.__username = username_cache[str(self.__uuid)]
-
+		self.__username = username
 
 	@property
 	def username(self) -> str:
 		return self.__username
 
+	@property
+	def total_mob_kills(self) -> int:
+		"""Get the total amount of kills done by this player."""
+		stats_file = get_server_file("world", "stats", f"{self.__uuid}.json")
+
+		with open(stats_file, mode="r") as f:
+			stats = jload(f)
+
+		try:
+			return stats['stat.mobKills']
+		except:
+			return 0
+
 	@staticmethod
-	def get_all_user_ids() -> List[str]:
-		player_folder = get_server_file("world", "playerdata", "gamestages")
+	def __load_username_cache() -> dict:
+		fpath = get_server_file("usernamecache.json")
 
-		__uuid_cache = [f[:-4] for f in listdir(player_folder)]
+		with open(fpath, mode='r') as f:
+			username_cache = jload(f)
 
-		return __uuid_cache
+		return username_cache
+
+	@classmethod
+	def get_all_players(cls):
+		all_users = cls.__load_username_cache()
+		return [cls(uuid, name) for uuid, name in all_users.items()]
+
+	@classmethod
+	def get_by_username(cls, username):
+		"""Create a player class from a given username."""
+		all_users = cls.__load_username_cache()
+
+		for uuid, uname in all_users.items():
+			if uname == username:
+				return cls(uuid, uname)
+
+		# If a user wasn't found, return an empty instance
+		return cls(None, None)
