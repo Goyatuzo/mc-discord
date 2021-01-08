85774a864471e650a43f5ed3cefb1f4abf25fcb9
Yuto Otaguro <yotagur@gmail.com>
2021-01-08 02:42:37 -0600

    Plot user kill as bar

--- mcdiscord/__main__.py
+++ mcdiscord/__main__.py
@@ -1,4 +1,5 @@
 import discord 
+import matplotlib.pyplot as plt 
 from os import environ
 from dotenv import load_dotenv
 from pathlib import Path
@@ -15,6 +16,8 @@ client = discord.Client()
 @client.event
 async def on_ready():
 	setup()
+
+
 	print('Bot is ready')
 
 
@@ -24,9 +27,18 @@ async def on_message(message):
 		players = Player.get_all_players()
 		players_by_lethality = sorted(players, key=attrgetter('total_mob_kills'), reverse=True)
 
-		top_three = players_by_lethality[:3]
-		to_respond = "\n".join([f"{player.username}: {player.total_mob_kills} mobs" for player in top_three])
-	
-		await message.channel.send(to_respond)
+
+		for player in players_by_lethality:
+			plot = plt.bar([0], player.total_mob_kills)
+			plot.set_label(player.username[1:] if player.username.startswith("_") else player.username)
+
+
+		plt.legend()
+		plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom= False)
+		plt.savefig('killer.png')
+		plt.clf()
+
+		await message.channel.send(file=discord.File('killer.png'))
+
 
 client.run(environ['MCD_BOT_TOKEN'])
