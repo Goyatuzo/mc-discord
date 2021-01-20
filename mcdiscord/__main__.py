50c7309c06f1edc7fe02e7346b34759d31cbf136
Yuto Otaguro <yotagur@gmail.com>
2021-01-20 09:41:21 -0600

    Killer refactor is done

--- mcdiscord/__main__.py
+++ mcdiscord/__main__.py
@@ -8,7 +8,7 @@ load_dotenv(verbose=True)
 
 from .schedule import setup_schedule
 from .servernet import setup
-from .graph import line_graph_stats
+from .graph import line_graph_single_stats, line_graph_distance_traveled
 
 client = discord.Client()
 
@@ -23,7 +23,10 @@ async def on_ready():
 @client.event
 async def on_message(message):
 	if message.content == '!killer':
-		image_file = line_graph_stats("mobKills", y_axis_label="Kills")
+		image_file = line_graph_single_stats("mobKills", y_axis_label="Kills")
+		await message.channel.send(file=discord.File(image_file))
+	elif message.content == "!explorer":
+		image_file = line_graph_distance_traveled()
 		await message.channel.send(file=discord.File(image_file))
 
 
--- mcdiscord/graph.py
+++ mcdiscord/graph.py
@@ -5,13 +5,13 @@ from time import time
 
 from .db import stats_collection
 
-def line_graph_stats(*stat_key: str, y_axis_label="") -> str:
-
+def __generate_graph_and_name(project_value: str, y_axis_label: str, fname: str="") -> str:
 	# Start timing
 	start_time = time()
 
 	plt.clf()
 
+	print(f"Graphing: {project_value}")
 	dat = pd.DataFrame(list(stats_collection.aggregate([{
 			"$lookup": {
 				"from": 'players',
@@ -25,21 +25,47 @@ def line_graph_stats(*stat_key: str, y_axis_label="") -> str:
 				"_id": 0,
 				"name": { "$arrayElemAt":  ["$player.name", 0] },
 				"date": 1,
-				"value": f"$stat.{'.'.join(stat_key)}._stat"
+				"value": project_value
 			}
 		}
 	])))
 
+	# Usernames that start with underscore break matplotlib, string escape yielded mixed results
 	dat["name"] = dat["name"].map(lambda name: name.lstrip("_") if name.startswith("_") else name)
 
-
+	# Begin constructing the graph
 	plt.figure()
+
+	# pandas gives an easier way of constructing a basic line graph
 	dat.pivot(index="date", columns="name", values="value").plot()
 	
+	# Set the labels accordingly
 	plt.xlabel("Date")
 	plt.ylabel(y_axis_label)
-	plt.savefig('killer.png')
 
-	print(f"Graphing: {'.'.join(stat_key)} took {time() - start_time} seconds")
+	# Finally save output file to system so Discord.py can send it
+	output_filename = f'{fname}.png'
+	plt.savefig(output_filename)
 
-	return "killer.png"
+	print(f"Graphing: {project_value} took {time() - start_time} seconds")
+
+	return output_filename
+
+def line_graph_single_stats(*stat_key: str, y_axis_label="") -> str:
+	return __generate_graph_and_name(f"$stat.{'.'.join(stat_key)}._stat", y_axis_label, y_axis_label)
+
+def line_graph_distance_traveled() -> str:
+	distance_stats = ["crouchOneCm",
+						"flyOneCm",
+						"diveOneCm",
+						"walkOneCm",
+						"runOneCm",
+						"fallOneCm",
+						"climbOneCm",
+						"boatOneCm",
+						"swimOneCm",
+						"horseOneCm"]
+
+	return __generate_graph_and_name({
+		"$add": distance_stats
+	}, "Distance Traveled", "distance")
