fb4973db237fc2ff479e2b6ec237d6be571e6ee9
Yuto Otaguro <yotagur@gmail.com>
2021-01-21 14:22:42 -0600

    Help text shows on error if split fails

--- mcdiscord/__main__.py
+++ mcdiscord/__main__.py
@@ -23,22 +23,26 @@ async def on_ready():
 async def on_message(message):
 	if not message.author.bot and message.content.startswith("!"):
 		if message.content.startswith("!graph"):
-			_, graph_type = message.content.split()
+			help_text = "Options are: killer, explorer, scrub, tank"
+			try:
+				_, graph_type = message.content.split()
 
-			if graph_type == 'killer':
-				image_file = line_graph_single_stats("mobKills", y_axis_label="Kills")
-				await message.channel.send(file=discord.File(image_file))
-			elif graph_type == "explorer":
-				image_file = line_graph_distance_traveled()
-				await message.channel.send(file=discord.File(image_file))
-			elif graph_type == "scrub":
-				image_file = line_graph_single_stats("deaths", y_axis_label="Deaths")
-				await message.channel.send(file=discord.File(image_file))
-			elif graph_type == "tank":
-				image_file = line_graph_single_stats("damageTaken", y_axis_label="Damage Taken")
-				await message.channel.send(file=discord.File(image_file))
-			else:
-				await message.channel.send("Options are: killer, explorer, scrub, tank")
+				if graph_type == 'killer':
+					image_file = line_graph_single_stats("mobKills", y_axis_label="Kills")
+					await message.channel.send(file=discord.File(image_file))
+				elif graph_type == "explorer":
+					image_file = line_graph_distance_traveled()
+					await message.channel.send(file=discord.File(image_file))
+				elif graph_type == "scrub":
+					image_file = line_graph_single_stats("deaths", y_axis_label="Deaths")
+					await message.channel.send(file=discord.File(image_file))
+				elif graph_type == "tank":
+					image_file = line_graph_single_stats("damageTaken", y_axis_label="Damage Taken")
+					await message.channel.send(file=discord.File(image_file))
+				else:
+					await message.channel.send(help_text)
+			except:
+				await message.channel.send(help_text)
 		else:
 			await message.channel.send("Valid Commands: !graph")
 
