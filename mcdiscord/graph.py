8e6302636e3cd8c622c4c2feef3c0c4c5be3fe1e
Yuto Otaguro <yotagur@gmail.com>
2021-01-21 14:14:00 -0600

    Close all figures each time just in case

--- mcdiscord/graph.py
+++ mcdiscord/graph.py
@@ -9,7 +9,7 @@ def __generate_graph_and_name(project_value: str, y_axis_label: str, fname: str=
 	# Start timing
 	start_time = time()
 
-	plt.clf()
+	plt.close('all')
 
 	print(f"Graphing: {y_axis_label}")
 	dat = pd.DataFrame(list(stats_collection.aggregate([{
