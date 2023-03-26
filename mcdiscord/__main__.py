6ec5627f2b0c0547be9e3d52b48acbb15fd543f7
Goyatuzo <yotagur@gmail.com>
2023-03-25 23:57:56 -0500

    Add to the list

--- mcdiscord/__main__.py
+++ mcdiscord/__main__.py
@@ -8,7 +8,8 @@ from discord.ext import tasks
 load_dotenv(verbose=True)
 
 from .graph import line_graph_single_stats, line_graph_distance_traveled
-from .schedule import player_sync 
+from .schedule import players
+from .schedule import stats
 from .servernet import setup
 
 
@@ -18,10 +19,16 @@ class McClient(discord.Client):
 
 	async def setup_hook(self) -> None:
 		self.player_sync.start()
+		self.player_stats_sync.start()
 
-	@tasks.loop(minutes=15)
+	@tasks.loop(minutes=10)
 	async def player_sync(self):
-		player_sync()
+		players.store_players_in_database()
+
+	@tasks.loop(minutes=10)
+	async def player_stats_sync(self):
+		stats.store_stats_in_database()
+
 
 	async def on_ready(self):
 		print('Bot is ready')
